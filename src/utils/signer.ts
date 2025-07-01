ff8d40564eeee025893cc14a4b683056acb7e1c8
destifo <estifanosbireda@gmail.com>
2025-07-01 14:19:08 +0300

    wip: sig fix

--- src/utils/signer.ts
+++ src/utils/signer.ts
@@ -370,8 +370,11 @@ export async function verifyV4Signature(
 
   const signableRequest = toSignableRequest(
     originalRequest,
-    originalSignature.signedHeaders,
+    [...originalSignature.signedHeaders],
   );
+  // console.log("original request", originalRequest);
+  // console.log("signableRequest", signableRequest);
+  // console.log("originalSignature", originalSignature);
   const signedRequest = originalSignature.source == "pre-sign"
     ? await signer.presign(signableRequest, {
       signableHeaders: new Set(originalSignature.signedHeaders),
@@ -411,6 +414,7 @@ export function toSignableRequest(
   signedHeaders: string[],
 ): HttpRequest {
   const reqUrl = new URL(req.url);
+  // console.log("***********", signedHeaders);
   const headersRecord = {} as Record<string, string>;
   req.headers.forEach((val, key) => {
     const lower = key.toLowerCase();
@@ -426,6 +430,7 @@ export function toSignableRequest(
     if (
       !lastIp || !isIpInMultipleCidrRanges(lastIp)
     ) {
+      // console.log("************", lastIp);
       logger.warn(
         "Request from untrusted IP",
         { lastIp, forwardedHost, headers: headersRecord },
