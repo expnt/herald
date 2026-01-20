9feb9c86da7356cadfa5ada5340b8645aac07048
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2026-01-20 01:35:11 +0100

    fix: give way to the new

--- src/utils/crypto.ts
+++
@@ -1,9 +0,0 @@
-import * as random from "std/random";
-
-export function getRandomInt(min = 0, max: number) {
-  return random.randomIntegerBetween(min, max);
-}
-
-export function getRandomUUID(): string {
-  return crypto.randomUUID();
-}
